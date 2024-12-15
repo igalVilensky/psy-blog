d88df7256c3f218d840f609581e8ac6f51c936ae
Igal <igal.vilensky@flowplace.com>
2024-12-15 11:23:12 +0100

    top menu and home page. add loading state with spinner

--- components/ui/TopBar.vue
+++ components/ui/TopBar.vue
@@ -1,21 +1,40 @@
 <template>
-  <nav class="bg-blue-600 text-white p-4">
-    <div class="container mx-auto flex justify-between items-center">
+  <nav class="text-black p-4">
+    <div
+      class="container max-w-screen-xl w-full mx-auto flex justify-between items-center"
+    >
       <!-- Brand/Logo -->
-      <div class="text-lg font-bold">Anastasia</div>
+      <NuxtLink
+        to="/"
+        class="text-2xl font-bold hover:text-purple-400 focus:outline-none"
+        aria-label="Home"
+      >
+        Anastasia
+      </NuxtLink>
 
       <!-- Desktop Menu -->
-      <ul class="hidden md:flex space-x-4">
+      <ul class="hidden md:flex space-x-8">
         <li>
-          <NuxtLink to="/" class="hover:underline hover:text-gray-200"
-            >Курсы</NuxtLink
+          <NuxtLink to="/" class="hover:underline"
+            >Бесплатные материалы</NuxtLink
           >
         </li>
         <li>
-          <NuxtLink to="/about" class="hover:underline">Гайды</NuxtLink>
+          <NuxtLink to="/about" class="hover:underline">Курсы</NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/contact" class="hover:underline">Блог</NuxtLink>
+          <NuxtLink to="/contact" class="hover:underline">Гайды</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/" class="hover:underline">Тесты</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about" class="hover:underline">Блог</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/contact" class="hover:underline"
+            >Обратная связь</NuxtLink
+          >
         </li>
       </ul>
 
@@ -29,19 +48,19 @@
         >
           <!-- Top Line -->
           <span
-            class="block w-6 h-0.5 bg-white transition-all duration-300 ease-in-out"
+            class="block w-6 h-0.5 bg-black transition-all duration-500 ease-in-out"
             :class="
               isDropdownOpen ? 'rotate-45 translate-y-2' : 'translate-y-0'
             "
           ></span>
           <!-- Middle Line -->
           <span
-            class="block w-6 h-0.5 bg-white mt-1.5 transition-all duration-300 ease-in-out"
+            class="block w-6 h-0.5 bg-black mt-1.5 transition-all duration-500 ease-in-out"
             :class="isDropdownOpen ? 'opacity-0' : 'opacity-100'"
           ></span>
           <!-- Bottom Line -->
           <span
-            class="block w-6 h-0.5 bg-white mt-1.5 transition-all duration-300 ease-in-out"
+            class="block w-6 h-0.5 bg-black mt-1.5 transition-all duration-500 ease-in-out"
             :class="
               isDropdownOpen ? '-rotate-45 -translate-y-2' : 'translate-y-0'
             "
@@ -51,37 +70,64 @@
         <!-- Dropdown Menu -->
         <ul
           :class="[
-            'absolute -right-4 mt-4 bg-white text-black rounded shadow-md w-40 overflow-hidden transition-all duration-500 ease-in-out',
+            'absolute -right-4 mt-4 bg-blue-400 py-4 rounded shadow-md w-40 overflow-hidden transition-all duration-700 ease-in-out transform',
             isDropdownOpen
-              ? 'max-h-96 opacity-100 scale-100'
+              ? 'max-h-[400px] opacity-100 scale-100'
               : 'max-h-0 opacity-0 scale-95',
           ]"
         >
-          <li class="border-b border-gray-200">
+          <li class="text-white">
             <NuxtLink
               to="/"
-              class="block px-4 py-2 hover:bg-gray-100"
+              class="block px-8 py-2 transition-colors duration-300 ease-in-out hover:bg-blue-500"
+              @click="closeDropdown"
+            >
+              Бесплатные материалы
+            </NuxtLink>
+          </li>
+          <li class="text-white">
+            <NuxtLink
+              to="/"
+              class="block px-8 py-2 transition-colors duration-500 ease-in-out hover:bg-blue-500"
               @click="closeDropdown"
             >
               Курсы
             </NuxtLink>
           </li>
-          <li class="border-b border-gray-200">
+          <li class="text-white">
             <NuxtLink
               to="/about"
-              class="block px-4 py-2 hover:bg-gray-100"
+              class="block px-8 py-2 transition-colors duration-500 ease-in-out hover:bg-blue-500"
               @click="closeDropdown"
             >
               Гайды
             </NuxtLink>
           </li>
-          <li>
+          <li class="text-white">
+            <NuxtLink
+              to="/about"
+              class="block px-8 py-2 transition-colors duration-500 ease-in-out hover:bg-blue-500"
+              @click="closeDropdown"
+            >
+              Тесты
+            </NuxtLink>
+          </li>
+          <li class="text-white">
             <NuxtLink
               to="/contact"
-              class="block px-4 py-2 hover:bg-gray-100"
+              class="block px-8 py-2 transition-colors duration-500 ease-in-out hover:bg-blue-500"
               @click="closeDropdown"
             >
               Блог
+            </NuxtLink>
+          </li>
+          <li class="text-white">
+            <NuxtLink
+              to="/contact"
+              class="block px-8 py-2 transition-colors duration-500 ease-in-out hover:bg-blue-500"
+              @click="closeDropdown"
+            >
+              Обратная связь
             </NuxtLink>
           </li>
         </ul>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,34 @@
 <template>
-  <div
-    class="flex gap-4 flex-col items-center bg-gray-100 py-4 min-h-screen mx-2"
-  >
-    <div class="bg-blue-400 w-full py-2 px-4 rounded-2xl">
-      <h3 class="text-white text-lg font-semibold">Популярные запросы</h3>
+  <div class="bg-gray-100 py-4 min-h-screen">
+    <!-- Simulate Loading State -->
+    <div v-if="isLoading" class="w-full flex justify-center items-center h-32">
+      <!-- Spinner -->
+      <div
+        class="animate-spin mt-24 rounded-full border-t-4 border-blue-400 border-solid w-16 h-16"
+      ></div>
     </div>
-    <div class="bg-purple-400 w-full py-2 px-4 rounded-2xl h-32">
-      <p class="text-xl font-bold text-white w-1/3">Любовь и отношения</p>
-    </div>
+    <div
+      v-else
+      class="max-w-screen-xl w-full mx-auto flex flex-col items-center gap-4 px-4 sm:px-0"
+    >
+      <div class="bg-blue-400 w-full py-2 px-4 rounded-2xl">
+        <h3 class="text-white text-lg font-semibold">Популярные запросы</h3>
+      </div>
+      <!-- Cards -->
+      <div class="flex w-full gap-4">
+        <div class="bg-purple-400 w-96 py-2 px-4 rounded-2xl h-32">
+          <p class="text-xl font-bold text-white w-24">Любовь и отношения</p>
+        </div>
+        <div class="bg-purple-400 w-96 py-2 px-4 rounded-2xl h-32">
+          <p class="text-xl font-bold text-white w-24">Любовь и отношения</p>
+        </div>
+      </div>
 
-    <div class="bg-blue-400 w-full py-2 px-4 rounded-2xl">
-      <h3 class="text-white text-lg font-semibold">Курсы недели со скидкой</h3>
+      <div class="bg-blue-400 w-full py-2 px-4 rounded-2xl">
+        <h3 class="text-white text-lg font-semibold">
+          Курсы недели со скидкой
+        </h3>
+      </div>
     </div>
     <!-- <p class="text-lg mt-4">
       <NuxtLink to="/about" class="text-blue-600 underline hover:text-blue-800">
@@ -19,3 +37,16 @@
     </p> -->
   </div>
 </template>
+<script setup>
+import { ref, onMounted } from "vue";
+
+// Simulate loading state
+const isLoading = ref(true);
+
+// Simulate loading for 0.5 second
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 500);
+});
+</script>
