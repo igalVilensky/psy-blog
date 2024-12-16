76e9da571fa4047d8cb1d109728ff2b66f21bd50
Igal <igal.vilensky@flowplace.com>
2024-12-16 02:08:46 +0100

    reset

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,19 @@
 <template>
-  <div class="overflow-hidden">
-    <div ref="topBar" class="top-bar">
-      <TopBar />
-    </div>
+  <div class="flex flex-col min-h-screen">
+    <!-- TopBar -->
+    <TopBar />
 
-    <main ref="mainContent" :style="{ height: `${mainHeight}px` }">
-      <NuxtPage />
+    <!-- Page Content (Main Content) -->
+    <main>
+      <NuxtPage class="flex-grow" />
     </main>
 
-    <div ref="footer" class="footer">
-      <Footer />
-    </div>
+    <!-- Footer -->
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import TopBar from "@/components/ui/TopBar.vue";
 import Footer from "@/components/ui/Footer.vue";
-
-const topBarHeight = ref(0);
-const footerHeight = ref(0);
-const mainHeight = ref(window.innerHeight);
-
-const updateHeights = () => {
-  const topBar = document.querySelector(".top-bar");
-  const footer = document.querySelector(".footer");
-
-  topBarHeight.value = topBar.offsetHeight;
-  footerHeight.value = footer.offsetHeight;
-  mainHeight.value =
-    window.innerHeight - (topBarHeight.value + footerHeight.value);
-};
-
-onMounted(() => {
-  updateHeights();
-  window.addEventListener("resize", updateHeights);
-});
-
-onUnmounted(() => {
-  window.removeEventListener("resize", updateHeights);
-});
 </script>
